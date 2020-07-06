1e7b42a4fbc0522425c971b5f3c06cb471e7aa6d
Kovarali <yannickov@gmail.com>
2020-07-06 11:03:31 -0500

    Updates

---
+++ precache-manifest.4dccd7d35d0702821b5b35e5edf58333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d48f47c8a8b853f5323c517e006b09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1547f131c98eb3bae188",
+    "url": "/static/css/main.fc294c8d.chunk.css"
+  },
+  {
+    "revision": "627f7ae81db3309f61d8",
+    "url": "/static/js/2.5f1d3220.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f1d3220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1547f131c98eb3bae188",
+    "url": "/static/js/main.93e3e6a5.chunk.js"
+  },
+  {
+    "revision": "ff2813303aeef512eed9",
+    "url": "/static/js/runtime-main.0c00b99d.js"
+  }
+]);
